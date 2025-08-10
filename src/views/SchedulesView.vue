d75148d165f05c2271c1a0f0e1b4bc552823888b
Renan Leite Vieira <renanleitev@gmail.com>
2025-08-10 16:33:22 -0300

    feat: Adiciona componente para listar agendamentos

--- src/views/SchedulesView.vue
+++ src/views/SchedulesView.vue
@@ -9,21 +9,53 @@
       @apply-filters="applyFilters"
       @clear-filters="clearFilters"
     />
+
+    <ScheduleList
+      :schedules="schedules"
+      :loading="loading"
+      :error="error"
+      :is-empty="isEmpty"
+      :has-filters="hasFilters"
+      :pagination="pagination"
+      :sort-order="sorting.sortOrder"
+      :can-go-prev="canGoPrev"
+      :can-go-next="canGoNext"
+      @toggle-sort="toggleSort"
+      @change-items-per-page="changeItemsPerPage"
+      @go-to-page="goToPage"
+      @prev-page="prevPage"
+      @next-page="nextPage"
+      @retry-fetch="fetchSchedules"
+    />
   </PageContainer>
 </template>
 
 <script setup lang="ts">
 import { onMounted } from 'vue'
 import ScheduleFilters from '../components/surgical-schedules/ScheduleFilters.vue'
+import ScheduleList from '../components/surgical-schedules/ScheduleList.vue'
 import { useSchedules } from '../composables/useSchedules'
 import PageContainer from '@/components/common/PageContainer.vue'
 
 const {
+  schedules,
+  loading,
+  error,
   filters,
+  pagination,
+  sorting,
   hasFilters,
+  isEmpty,
+  canGoNext,
+  canGoPrev,
   fetchSchedules,
   applyFilters,
   clearFilters,
+  goToPage,
+  nextPage,
+  prevPage,
+  toggleSort,
+  changeItemsPerPage,
 } = useSchedules()
 
 onMounted(() => {
